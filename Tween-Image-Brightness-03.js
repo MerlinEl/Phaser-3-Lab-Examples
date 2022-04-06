2c6f201f0e7e9e71438724143f4a5da69275db97
MerlinEl (Orien) <merlin_el@hotmail.com>
2022-04-06 13:27:45 +0200

    up

---
+++ Tween-Image-Brightness-03.js
@@ -0,0 +1,36 @@
+/**
+ * @author       IcyFire <merlin_el@hotmail.com>
+ * @copyright    2022
+ * @license      {@link https://opensource.org/licenses/MIT|MIT License}
+ */
+var config = {
+    type: Phaser.WEBGL,
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create
+    },
+    width: 800,
+    height: 600
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    
+    this.load.image('lulu', 'assets/pics/shocktroopers-lulu2.png');
+}
+
+function create() {
+
+	// add both images in scene
+    var lulu1 = this.add.image(400, 300, 'lulu').setScale(1.5);
+    var lulu2 = this.add.image(400, 300, 'lulu').setScale(1.5).setTintFill(0xffffff);
+    this.tweens.add({
+        targets:lulu2,
+        alpha:0,
+        duaration:2000,
+        yoyo:true,
+        repeat:-1
+    })
+}
