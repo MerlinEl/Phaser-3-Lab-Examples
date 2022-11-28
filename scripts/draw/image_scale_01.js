b0d1642e51c4bbd188cd29f411c99a20e6c2b7db
MerlinEl (Orien) <merlin_el@hotmail.com>
2022-11-28 12:14:58 +0100

    Create image_scale_01.js

---
+++ scripts/draw/image_scale_01.js
@@ -0,0 +1,35 @@
+var config = {
+    type: Phaser.AUTO,
+    backgroundColor: '#2dab2d',
+    scale: {
+        mode: Phaser.Scale.NONE,
+        parent: 'phaser-example',
+        width: 1024,
+        height: 1024
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('pic', 'assets/pics/zero-two.png');
+}
+
+function create ()
+{
+    var img1 = this.add.image(0, 0, 'pic').setOrigin(0);
+    var img2 = this.add.image(0, 600, 'pic').setOrigin(0);
+    var img3 = this.add.image(0, 900, 'pic').setOrigin(0);
+    img2.setScale(.5, .5);
+    img3.setScale(.2, .2);
+
+    var tf =  { font: "24px Arial", fill: "#ffffff", align: "center" };
+    var txt1 = this.add.text(10, 10, '100% Scale', tf);
+    var txt2 = this.add.text(10, 610, '50% Scale', tf);
+    var txt3 = this.add.text(10, 910, '20% Scale', tf);
+}
