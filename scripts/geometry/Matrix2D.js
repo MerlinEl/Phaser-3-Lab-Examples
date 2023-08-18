a94d9d62c7ae2d46bfc103e706f9ce1175f2a9ce
MerlinEl (Orien) <merlin_el@hotmail.com>
2023-08-18 13:58:56 +0200

    Update Matrix2D.js

--- scripts/geometry/Matrix2D.js
+++ scripts/geometry/Matrix2D.js
@@ -1,3 +1,6 @@
+// ******************************* //
+//#region Point2D
+// ******************************* //
 class Point2D {
     x = 0;
     y = 0;
@@ -9,10 +12,16 @@ class Point2D {
         return "Point2D( x:" + this.x + ", y:" + this.y + ")";
     }
 }
+//#endregion
 
+// ******************************* //
+//#region Triangle2D
+// ******************************* //
 class Triangle2D {
     /** @type {Point2D} */
-    p1; p2; p3;
+    p1;
+    p2;
+    p3;
     constructor(p1, p2, p3) {
         this.p1 = p1 || new Point2D();
         this.p2 = p2 || new Point2D();
@@ -22,3 +31,25 @@ class Triangle2D {
         return "Triangle2D(\n\tp1:" + this.p1 + ",\n\tp2:" + this.p2 + ",\n\tp3:" + this.p3 + ")";
     }
 }
+//#endregion
+
+// ******************************* //
+//#region Trapezoid2D
+// ******************************* //
+class Trapezoid2D {
+    /** @type {Point2D} */
+    p1;
+    p2;
+    p3;
+    p4;
+    constructor(p1, p2, p3) {
+        this.p1 = p1 || new Point2D();
+        this.p2 = p2 || new Point2D();
+        this.p3 = p3 || new Point2D();
+        this.p4 = p4 || new Point2D();
+    }
+    toString() {
+        return "Triangle2D(\n\tp1:" + this.p1 + ",\n\tp2:" + this.p2 + ",\n\tp3:" + this.p3 + ",\n\tp4:" + this.p4 + ")";
+    }
+}
+//#endregion
