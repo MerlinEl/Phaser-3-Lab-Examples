575f38c7bf7e63257b7023d86f6a33d44a5a7283
MerlinEl (Orien) <merlin_el@hotmail.com>
2023-08-18 11:42:25 +0200

    Update draw_gradient_star_01.js

--- scripts/draw/draw_gradient_star_01.js
+++ scripts/draw/draw_gradient_star_01.js
@@ -1,37 +1,76 @@
+class Point2D {
+    x = 0;
+    y = 0;
+    constructor(x, y) {
+        this.x = x || 0;
+        this.y = y || 0;
+    }
+    toString() {
+        return "Point2D( x:" + this.x + ", y:" + this.y + ")";
+    }
+}
+
+class Triangle2D {
+    /** @type {Point2D} */
+    p1; p2; p3;
+    constructor(p1, p2, p3) {
+        this.p1 = p1 || new Point2D();
+        this.p2 = p2 || new Point2D();
+        this.p3 = p3 || new Point2D();
+    }
+    toString() {
+        return "Triangle2D(\n\tp1:" + this.p1 + ",\n\tp2:" + this.p2 + ",\n\tp3:" + this.p3 + ")";
+    }
+}
+
 class GradientStar extends Phaser.Scene {
     
+    constructor () {super(); }
+    preload () {}
     create() {
         var g = this.make.graphics({x: 0, y: 0, add: false});
-        this.drawStar(g, {x:105, y:105},  5, 100, 50, 0xFFFF00, 0xFF0000, 5);
+        this.drawStar(g, new Point2D(105, 105),  5, 100, 50, 0xFFFF00, 0xFF0000);
         g.generateTexture('starGraphics', 210, 210);
         var image = this.add.image(400, 300, 'starGraphics');
     }
 
-    drawStar (g, center, spikes, outerRadius, innerRadius, fillColor, lineColor, lineWidth) {
-        var rot = (Math.PI / 2) * 3;
+    drawStar (g, center, spikes, outerRadius, innerRadius, inner_color, outer_color) {
+        var a = (Math.PI / 2) * 3;
         var x = center.x;
         var y = center.y;
         var step = Math.PI / spikes;
-        if (lineColor != null) g.lineStyle(lineWidth, lineColor, 1.0);
-        g.fillStyle(fillColor, 1.0);
-        g.beginPath();
-        g.moveTo(center.x, center.y - outerRadius);
-        for (var i = 0; i < spikes; i++) {
-            x = center.x + Math.cos(rot) * outerRadius;
-            y = center.y + Math.sin(rot) * outerRadius;
-            g.lineTo(x, y);
-            rot += step;
+        //var points = [new Point2D(center.x, center.y - outerRadius)];
 
-            x = center.x + Math.cos(rot) * innerRadius;
-            y = center.y + Math.sin(rot) * innerRadius;
-            g.lineTo(x, y);
-            rot += step;
+        var triangles = [];
+        for (var i = 0; i < spikes; i++) {
+            
+            var p2 = new Point2D(
+                center.x + Math.cos(a) * outerRadius,
+                center.y + Math.sin(a) * outerRadius
+            )
+            a += step;
+
+            var p3 = new Point2D(
+                center.x + Math.cos(a) * innerRadius,
+                center.y + Math.sin(a) * innerRadius
+            )
+            a += step;
+
+            triangles.push(new Triangle2D(center, p2, p3))
         }
-        g.lineTo(center.x, center.y - outerRadius);
-        g.closePath();
-        g.fillPath();
-        if (lineColor != null) g.strokePath();
+        //g.lineTo(center.x, center.y - outerRadius);
+        // draw triangles from points
+        triangles.forEach((t, i) => {
+            if (i < spikes) {
+                var p1 = t.p1;
+                var p2 = t.p2;
+                var p3 = t.p3;
+                g.fillGradientStyle(inner_color, outer_color, outer_color, inner_color, 1);
+                g.fillTriangle(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
+            }
+        });
     };
+    update () {}
 }
 
 const config = {
