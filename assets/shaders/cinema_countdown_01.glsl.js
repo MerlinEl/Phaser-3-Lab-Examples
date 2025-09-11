8a13b6b199f249efef651065e8bc5aed381bf3bc
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-11 19:44:55 +0200

    Create cinema_countdown_01.glsl.js

---
+++ assets/shaders/cinema_countdown_01.glsl.js
@@ -0,0 +1,18 @@
+  precision mediump float;
+
+  uniform sampler2D uMainTexture;
+  uniform float uRadius;
+  uniform vec2 resolution;
+  
+  varying vec2 outTexCoord;
+  void main() {
+      vec2 center = vec2(0.5);
+      float aspectRatio = resolution.x / resolution.y;
+      vec2 aspectCorrectedCoord = vec2(outTexCoord.x, outTexCoord.y / aspectRatio);
+      float distance = distance(aspectCorrectedCoord, center);
+      
+      vec4 texColor = texture2D(uMainTexture, outTexCoord);
+      
+      float alpha = 1.0 - step(uRadius, distance);
+      gl_FragColor = vec4(texColor.rgb, texColor.a * alpha);
+  }
