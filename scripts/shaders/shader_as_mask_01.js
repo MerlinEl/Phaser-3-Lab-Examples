bf6fe94daee2c6ec56b5c21e9141c37b6f9511f5
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-11 19:00:55 +0200

    Create shader_as_mask_01.js

---
+++ scripts/shaders/shader_as_mask_01.js
@@ -0,0 +1,40 @@
+var config = {
+    type: Phaser.WEBGL,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.setBaseURL('https://raw.githubusercontent.com/MerlinEl/Phaser-3-Lab-Examples/main/assets');
+    this.load.glsl('wave', '/shaders/shader1.frag');
+    this.load.image('pic', '/pics/sao-sinon.png');
+    this.load.image('bg', '/pics/purple-dots.png');
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'bg');
+
+    var shader = this.make.shader({
+        key: 'wave',
+        x: 400,
+        y: 300,
+        width: 800,
+        height: 600,
+        add: false
+    });
+
+    //  Make a Bitmap Mask from it
+    var mask = shader.createBitmapMask();
+
+    //  Apply the mask to this image
+    this.add.image(400, 300, 'pic').setMask(mask);
+}
