b624efe5e9fb3ebf943426ac9db633a41c9900e5
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-11 17:17:40 +0200

    Create cinema_countdown_01.js

---
+++ assets/frag/cinema_countdown_01.js
@@ -0,0 +1,37 @@
+// GLSL Shader code (radial_reveal.frag)
+// shader must be on same path
+class CinemaCountdown extends Phaser.Renderer.WebGL.Pipelines.SinglePipeline {
+    constructor(game) {
+        super({
+            game,
+            fragShader: `
+            precision mediump float;
+            uniform sampler2D uMainTexture;
+            uniform float uRadius;
+            uniform vec2 uResolution;
+            varying vec2 outTexCoord;
+        
+            void main() {
+                vec2 center = vec2(0.5, 0.5);
+                float aspectRatio = uResolution.x / uResolution.y;
+                vec2 aspectCorrectedCoord = vec2(outTexCoord.x, outTexCoord.y * aspectRatio);
+                float distance = distance(aspectCorrectedCoord, center);
+        
+                vec4 color = texture2D(uMainTexture, outTexCoord);
+        
+                float alpha = 1.0 - step(uRadius, distance); // Invert step to reveal
+                gl_FragColor = vec4(color.rgb, color.a * alpha);
+            }`,
+        });
+        this._u_radius = 0.5;
+    }
+    onPreRender() {
+        this.set1f('uRadius', this._u_radius);
+    }
+    get radius() {
+        return this._color;
+    }
+    set radius (val){
+        this._u_radius = val;
+    }
+}
