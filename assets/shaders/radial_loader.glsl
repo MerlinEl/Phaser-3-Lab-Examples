99163938556f0723cbc05a840a9ac7138bc31839
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-14 21:44:25 +0200

    Update radial_loader.glsl

--- assets/shaders/radial_loader.glsl
+++ assets/shaders/radial_loader.glsl
@@ -2,43 +2,29 @@
 precision mediump float;
 #endif
 
-uniform float time;
-uniform vec2 resolution;
+uniform float time;        // animace
+uniform vec2 resolution;   // velikost plátna
 
-void main( void ) {
-    // Normalizujeme souřadnice od -1 do 1 a posuneme je do středu
-    vec2 pos1 = gl_FragCoord.xy / resolution.xy - 0.5;
-    
-    // Udržujeme poměr stran
-    pos1.x *= resolution.x / resolution.y;
-    
-    // Vypočítáme vzdálenost (r) a úhel (angle)
-    float r = length(pos1);
-    float angle = atan(pos1.y, pos1.x);
+void main(void) {
+    // Normalizované souřadnice kolem středu
+    vec2 uv = (gl_FragCoord.xy - 0.5 * resolution.xy) / resolution.y;
+    float r = length(uv);            // vzdálenost od středu
+    float angle = atan(uv.y, uv.x);  // úhel v rozsahu -PI..PI
 
-    // Normalizujeme úhel na rozsah od 0 do 2*PI
-    angle = mod(angle, 6.28318530718);
+    // normalizace úhlu na 0..1
+    float normAngle = fract(angle / (2.0 * 3.14159265) + 0.5);
 
-    // Vypočítáme animovanou hodnotu (progress)
-    float progress = mod(time / 2.0, 1.0) * 6.28318530718;
+    // "procenta" loaderu (0..1 cyklující podle času)
+    float progress = fract(time * 0.2); 
 
-    // Vytvoříme plný kruh
-    float circle = step(r, 0.4);
+    // Tloušťka prstence
+    float inner = 0.3;
+    float outer = 0.4;
 
-    // Vytvoříme vnitřní kruh, abychom z kruhu udělali prstenec
-    float innerCircle = step(0.3, r);
-
-    // Sestavíme prstenec, který je viditelný jen v rozsahu od 0.3 do 0.4
-    float ring = circle * innerCircle;
-
-    // Zkontrolujeme, jestli je úhel (angle) menší než náš progress
-    float loadingArc = step(angle, progress);
-
-    // Vykreslíme jen tu část prstence, která odpovídá načítání
-    float result = ring * loadingArc;
-    
-    // Nyní to klíčové: nastavujeme alfa kanál
-    // Pokud je "result" 1.0, alfa bude také 1.0 (plně viditelná)
-    // Pokud je "result" 0.0, alfa bude také 0.0 (plně průhledná)
-    gl_FragColor = vec4(1.0, 1.0, 1.0, result);
+    // Podmínky: uvnitř prstence a menší úhel než progress
+    if (r > inner && r < outer && normAngle < progress) {
+        gl_FragColor = vec4(1.0); // bílý loader
+    } else {
+        gl_FragColor = vec4(0.0); // průhledné/černé pozadí
+    }
 }
