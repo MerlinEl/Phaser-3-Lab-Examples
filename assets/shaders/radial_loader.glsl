45c83fd84ada08c706e3bba19319ffd8e8d5f838
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-14 22:00:01 +0200

    Update radial_loader.glsl

--- assets/shaders/radial_loader.glsl
+++ assets/shaders/radial_loader.glsl
@@ -1,15 +1,13 @@
-#ifdef GL_ES
 precision mediump float;
-#endif
 
-uniform float progress;    // 0.0 - 1.0 (stav načítání)
-uniform vec2 resolution;   // velikost plátna
+uniform vec2      resolution;
+uniform float     progress;   // 0.0 - 1.0
 
 void main(void) {
     // Normalizované souřadnice kolem středu
     vec2 uv = (gl_FragCoord.xy - 0.5 * resolution.xy) / resolution.y;
-    float r = length(uv);            // vzdálenost od středu
-    float angle = atan(uv.y, uv.x);  // úhel v rozsahu -PI..PI
+    float r = length(uv);            
+    float angle = atan(uv.y, uv.x);  
 
     // normalizace úhlu na 0..1
     float normAngle = fract(angle / (2.0 * 3.14159265) + 0.5);
@@ -18,10 +16,10 @@ void main(void) {
     float inner = 0.3;
     float outer = 0.4;
 
-    // Podmínky: uvnitř prstence a menší úhel než progress
+    float alpha = 0.0;
     if (r > inner && r < outer && normAngle < progress) {
-        gl_FragColor = vec4(1.0); // bílý viditelný kus loaderu
-    } else {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0); // průhledné pozadí (pro masku)
+        alpha = 1.0;
     }
+
+    gl_FragColor = vec4(1.0, 1.0, 1.0, alpha); // DŮLEŽITÉ: alfa určuje masku
 }
