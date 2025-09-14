e7c06b59f34b897582f507a79c3c838dab2595c5
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-14 21:18:15 +0200

    Create radial_loader.glsl

---
+++ assets/shaders/radial_loader.glsl
@@ -0,0 +1,44 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform float time;
+uniform vec2 resolution;
+
+void main( void ) {
+    // Normalizujeme souřadnice od -1 do 1 a posuneme je do středu
+    vec2 pos1 = gl_FragCoord.xy / resolution.xy - 0.5;
+    
+    // Udržujeme poměr stran
+    pos1.x *= resolution.x / resolution.y;
+    
+    // Vypočítáme vzdálenost (r) a úhel (angle)
+    float r = length(pos1);
+    float angle = atan(pos1.y, pos1.x);
+
+    // Normalizujeme úhel na rozsah od 0 do 2*PI
+    angle = mod(angle, 6.28318530718);
+
+    // Vypočítáme animovanou hodnotu (progress)
+    // Tady můžete upravit rychlost a směr animace
+    float progress = mod(time / 2.0, 1.0) * 6.28318530718;
+
+    // Vytvoříme plný kruh
+    float circle = step(r, 0.4);
+
+    // Vytvoříme vnitřní kruh, abychom z kruhu udělali prstenec
+    float innerCircle = step(0.3, r);
+
+    // Sestavíme prstenec, který je viditelný jen v rozsahu od 0.3 do 0.4
+    float ring = circle * innerCircle;
+
+    // Zkontrolujeme, jestli je úhel (angle) menší než náš progress
+    float loadingArc = step(angle, progress);
+
+    // Vykreslíme jen tu část prstence, která odpovídá načítání
+    float result = ring * loadingArc;
+    
+    // Nakonec nastavíme barvu pixelu na základě výsledku
+    // Zelená barva pro načítání
+    gl_FragColor = vec4(vec3(0.0, 1.0, 0.0) * result, 1.0);
+}
