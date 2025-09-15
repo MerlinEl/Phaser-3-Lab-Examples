03a645a7a743969c531689ef998c604ed2eb57fc
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-15 09:05:36 +0200

    Update radial_loader_02.glsl

--- assets/shaders/radial_loader_02.glsl
+++ assets/shaders/radial_loader_02.glsl
@@ -1,9 +1,7 @@
 // Radial Loader Shader (Phaser 3 compatible)
 
-precision mediump float;
-
 // ==== VERTEX SHADER ====
-#ifdef VERTEX
+precision mediump float;
 
 uniform mat4 uProjectionMatrix;
 uniform mat4 uViewMatrix;
@@ -17,16 +15,14 @@ varying vec2 outTexCoord;
 void main () {
     gl_Position = uProjectionMatrix * uViewMatrix * vec4(inPosition, 1.0, 1.0);
 
-    // Přepočet souřadnic (Phaser styl)
     fragCoord = vec2(inPosition.x, uResolution.y - inPosition.y);
-
     outTexCoord = vec2(inPosition.x / uResolution.x, fragCoord.y / uResolution.y);
 }
 
-#endif
+---
 
 // ==== FRAGMENT SHADER ====
-#ifdef FRAGMENT
+precision mediump float;
 
 uniform vec2  uResolution;
 uniform float progress;   // 0.0 – 1.0 (stav loaderu)
@@ -34,25 +30,21 @@ uniform float progress;   // 0.0 – 1.0 (stav loaderu)
 varying vec2 fragCoord;
 
 void main(void) {
-    // Souřadnice kolem středu (normalizované)
+    // Souřadnice kolem středu
     vec2 uv = (fragCoord - 0.5 * uResolution) / uResolution.y;
 
     float r = length(uv);
     float angle = atan(uv.y, uv.x);
 
-    // úhel 0..1 (0 = vpravo, 0.25 = nahoře, 0.5 = vlevo, 0.75 = dole)
+    // Úhel normalizovaný do 0..1
     float normAngle = fract(angle / (2.0 * 3.14159265) + 0.5);
 
-    // Poloměr loaderu
     float radius = 0.4;
-
     float alpha = 0.0;
+
     if (r < radius && normAngle < progress) {
         alpha = 1.0;
     }
 
-    // Alfa určuje masku
     gl_FragColor = vec4(1.0, 1.0, 1.0, alpha);
 }
-
-#endif
