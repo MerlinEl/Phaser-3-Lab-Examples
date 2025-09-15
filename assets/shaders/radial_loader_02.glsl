70fe95b06fe3042fcddfc0926bf96ac826289a25
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-15 08:35:04 +0200

    Update radial_loader_02.glsl

--- assets/shaders/radial_loader_02.glsl
+++ assets/shaders/radial_loader_02.glsl
@@ -1,10 +1,10 @@
 precision mediump float;
 
-uniform vec2  uResolution;
+uniform vec2  resolution;
 uniform float progress;   // 0.0 – 1.0
 
 void main(void) {
-    vec2 uv = (gl_FragCoord.xy - 0.5 * uResolution.xy) / uResolution.y;
+    vec2 uv = (gl_FragCoord.xy - 0.5 * resolution.xy) / resolution.y;
     float r = length(uv);
     float angle = atan(uv.y, uv.x);
 
