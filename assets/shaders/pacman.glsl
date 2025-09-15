5048203f5c9ff107ea19d3052bd10b8d9ca22b77
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-15 16:45:37 +0200

    Update pacman.glsl

--- assets/shaders/pacman.glsl
+++ assets/shaders/pacman.glsl
@@ -9,11 +9,12 @@ precision mediump float;
 
 uniform mat4 uProjectionMatrix;
 uniform mat4 uViewMatrix;
-
 attribute vec2 inPosition;
+varying vec2 vUV;
 
 void main(void) {
     gl_Position = uProjectionMatrix * uViewMatrix * vec4(inPosition, 1.0, 1.0);
+    vUV = inPosition;          // Phaser už normalizuje do 0..1
 }
 
 ---
@@ -25,27 +26,24 @@ type: fragment
 precision mediump float;
 #endif
 
-uniform float mouthAngle; // úhel "úst" v radiánech
-uniform vec2  uResolution; // velikost shader plochy (např. 400x400)
-// version 0.01
+uniform float mouthAngle;   // úhel otevření (radiany)
+varying vec2 vUV;
+// v1.02
 void main(void) {
-    // souřadnice pixelu v rozsahu -1..1 kolem středu
-    vec2 uv = (gl_FragCoord.xy / uResolution) * 2.0 - 1.0;
+    // převedeme do rozsahu -1..1 kolem středu
+    vec2 uv = vUV * 2.0 - 1.0;
 
     float r = length(uv);
     float angle = atan(uv.y, uv.x);
+    if (angle < 0.0) angle += 6.2831853; // na rozsah 0..2π
 
-    if (angle < 0.0) angle += 6.2831853; // převedeme na 0..2π
-
-    float radius = 0.8;
     float alpha = 0.0;
-
-    if (r < radius) {
-        // vyřízneme "ústa" podle mouthAngle
+    if (r < 1.0) {
+        // vyřízneme klín (ústa)
         if (!(angle > 6.2831853 - mouthAngle || angle < mouthAngle)) {
             alpha = 1.0;
         }
     }
 
-    gl_FragColor = vec4(0.0, 0.0, 0.0, alpha); // černý pacman
+    gl_FragColor = vec4(0.0, 0.0, 0.0, alpha);
 }
