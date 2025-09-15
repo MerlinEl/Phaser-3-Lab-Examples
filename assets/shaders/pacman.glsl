e2a078450ec14115d9bc7f52f1f8dd05a547d602
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-15 16:57:22 +0200

    Update pacman.glsl

--- assets/shaders/pacman.glsl
+++ assets/shaders/pacman.glsl
@@ -32,23 +32,26 @@ precision mediump float;
 
 uniform float mouthAngle;   // úhel otevření (radiany)
 varying vec2 vUV;
-//v1.04
+//1.05
 void main(void) {
     // souřadnice -1..1 kolem středu
     vec2 uv = vUV * 2.0 - 1.0;
 
     float r = length(uv);
-    float angle = atan(uv.y, uv.x);
-    if (angle < 0.0) angle += 6.2831853; // na rozsah 0..2π
+    float angle = atan(uv.y, uv.x);     // -PI .. +PI
+    if (angle < 0.0) angle += 6.2831853; // 0 .. 2PI
 
     float alpha = 0.0;
+
     if (r < 1.0) {
-        // Pacman kouše směrem doprava (ústa u osy X)
+        // Pacman má ústa směrem doprava (osa X)
+        // Vyřízneme klín ±mouthAngle kolem 0 rad
         if (angle > mouthAngle && angle < (6.2831853 - mouthAngle)) {
-            alpha = 1.0;
+            alpha = 1.0;   // tělo
+        } else {
+            alpha = 0.0;   // pusa (průhledné)
         }
     }
 
     gl_FragColor = vec4(0.0, 0.0, 0.0, alpha);
 }
-
