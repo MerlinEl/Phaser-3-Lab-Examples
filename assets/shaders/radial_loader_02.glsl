4e8d3d9a77b284d68d268b1aec19022e40a373ea
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-15 09:14:08 +0200

    Update radial_loader_02.glsl

--- assets/shaders/radial_loader_02.glsl
+++ assets/shaders/radial_loader_02.glsl
@@ -1,7 +1,11 @@
-// Radial Loader Shader (Phaser 3 compatible)
+// Radial Loader Shader
 
-// ==== VERTEX SHADER ====
+#ifdef GL_ES
 precision mediump float;
+#endif
+
+// === VERTEX ===
+#define SHADER_VERTEX
 
 uniform mat4 uProjectionMatrix;
 uniform mat4 uViewMatrix;
@@ -12,7 +16,7 @@ attribute vec2 inPosition;
 varying vec2 fragCoord;
 varying vec2 outTexCoord;
 
-void main () {
+void main(void) {
     gl_Position = uProjectionMatrix * uViewMatrix * vec4(inPosition, 1.0, 1.0);
 
     fragCoord = vec2(inPosition.x, uResolution.y - inPosition.y);
@@ -21,22 +25,24 @@ void main () {
 
 ---
 
-// ==== FRAGMENT SHADER ====
+// === FRAGMENT ===
+#define SHADER_FRAGMENT
+
+#ifdef GL_ES
 precision mediump float;
+#endif
 
 uniform vec2  uResolution;
-uniform float progress;   // 0.0 – 1.0 (stav loaderu)
+uniform float progress;   // 0.0 – 1.0
 
 varying vec2 fragCoord;
 
 void main(void) {
-    // Souřadnice kolem středu
     vec2 uv = (fragCoord - 0.5 * uResolution) / uResolution.y;
 
     float r = length(uv);
     float angle = atan(uv.y, uv.x);
 
-    // Úhel normalizovaný do 0..1
     float normAngle = fract(angle / (2.0 * 3.14159265) + 0.5);
 
     float radius = 0.4;
