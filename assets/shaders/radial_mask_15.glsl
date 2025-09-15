98cfdcf7f8c4d2b3bdeea8be8a36af6bb2ba6227
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-15 20:53:48 +0200

    Update and rename radial_mask_14.glsl to radial_mask_15.glsl

--- assets/shaders/radial_mask_15.glsl
+++ assets/shaders/radial_mask_15.glsl
@@ -9,16 +9,14 @@ precision mediump float;
 
 uniform mat4 uProjectionMatrix;
 uniform mat4 uViewMatrix;
-uniform vec2 resolution;
-
+uniform vec2 uResolution;
 attribute vec2 inPosition;
 varying vec2 fragCoord;
-
-void main(void) {
-    gl_Position = uProjectionMatrix * uViewMatrix * vec4(inPosition, 1.0, 1.0);
-
-    // posíláme pozici přímo do fragmentu jako pixelové souřadnice
-    fragCoord = inPosition;
+varying vec2 outTexCoord;
+void main () {
+   gl_Position = uProjectionMatrix * uViewMatrix * vec4(inPosition, 1.0, 1.0);
+   fragCoord = vec2(inPosition.x, uResolution.y - inPosition.y);
+   outTexCoord = vec2(inPosition.x / uResolution.x, fragCoord.y / uResolution.y);
 }
 
 ---
