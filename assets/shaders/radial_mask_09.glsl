abbf21d6ddff65745e9ecf759216e8459ee21b13
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-15 20:38:36 +0200

    Update and rename radial_mask_06.glsl to radial_mask_09.glsl

--- assets/shaders/radial_mask_09.glsl
+++ assets/shaders/radial_mask_09.glsl
@@ -25,7 +25,7 @@ void main(void) {
 
 name: pacman
 type: fragment
-uniform.duration: { "type": "1f", "value": 3000 }   // default 3000 ms
+uniform.duration: { "type": "1f", "value": 1000 }   // default 3000 ms
 ---
 
 #ifdef GL_ES
