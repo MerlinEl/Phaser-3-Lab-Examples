19a2d10ee82cde82ec28f6ab508ccbf4e5a1a796
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-15 20:31:31 +0200

    Update and rename radial_mask_06.glsl to radial_mask_08.glsl

--- assets/shaders/radial_mask_08.glsl
+++ assets/shaders/radial_mask_08.glsl
@@ -6,9 +6,7 @@ type: fragment
 uniform.duration: { "type": "1f", "value": 3000 }   // default 3000 ms
 ---
 
-#ifdef GL_ES
 precision mediump float;
-#endif
 
 uniform vec2  resolution;
 uniform float time;       // Phaser dodává v sekundách
