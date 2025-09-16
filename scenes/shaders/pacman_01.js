c6976c1f86f35a9882d1ca838bf90c8c73b31d55
MerlinEl (Orien) <merlin_el@hotmail.com>
2025-09-16 09:19:47 +0200

    Update pacman_01.js

--- scenes/shaders/pacman_01.js
+++ scenes/shaders/pacman_01.js
@@ -14,13 +14,20 @@ var game = new Phaser.Game(config);
 
 function preload() {
     this.load.setBaseURL('https://raw.githubusercontent.com/MerlinEl/Phaser-3-Lab-Examples/main/assets');
-    this.load.glsl('bundle', '/shaders/pacman.glsl');
+    //this.load.glsl('bundle', '/shaders/pacman.glsl');
+    this.load.image('bg', '/images/purple-dots.png');
+    this.load.glsl('pacman', '/shaders/radial_mask_18.glsl');
 }
 
 function create() {
+    this.add.image(400, 300, 'bg');
     let pacman = this.add.shader('pacman', 400, 300, 400, 400);
-    // nastavíme úhel otevřených úst (radiany)
-    pacman.setUniform('mouthAngle.value', Math.PI / 6); // cca 30°
+    pacman.setUniform('duration.value', 5000);
+
+    console.log("pacman:",pacman);
+    console.log("shader:",pacman.shader);
+    console.log("frag:",pacman.shader.fragmentSrc);
+    
 }
 
 function update(){}
